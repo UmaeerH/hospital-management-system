57b5edf50801760eeeeeeb2ba6736728b8d04b5a
Umi <umaeerh04@gmail.com>
2024-01-02 13:41:50 +0000

    Doctor class

--- Doctor.py
+++ Doctor.py
@@ -1,10 +1,14 @@
+import itertools
+
 class Doctor:
+    newid = itertools.count()
     def __init__(self, fName, sName, speciality):
         self.__firstName = fName
         self.__secondName = sName
         self.__speciality = speciality
         self.__patients = []
         self.__appointments = []
+        self.__docID = next(self.newid)
 
     def get_firstName(self):
         return self.__firstName
@@ -18,18 +22,27 @@ class Doctor:
         return self.__speciality
     def set_speciality(self, newSpec):
         self.__speciality = newSpec
-    
+    def get_docID(self):
+        return self.__docID
 
     def add_patient(self, patient):
-        self.patients.append(patient)
+        self.__patients.append(patient)
+
+    def add_appointment(self, date):
+        self.__appointments.append(date)
 
     def __str__(self) :
-        return f'{self.__firstName:<10}{self.__secondName:<10}|{self.__speciality:^15}'
+        return f'{self.__firstName:<10}{self.__secondName:<10}|{self.__speciality:^15}|{self.__docID:^5}'
     
 # testing
-doc1 = Doctor("Umi", "Fudge", "Scientist")
-doc2 = Doctor("Tokai", "Teio", "Horse Vet")
-print(doc1)
-print(doc2)
-doc2.set_speciality("Retired")
-print(doc2)
+def main():
+    doc1 = Doctor("Ruby", "Armstrong", "Scientist")
+    doc2 = Doctor("Timothy", "Lopez", "GP")
+    doc2.set_speciality("Retired")
+    doc3 = Doctor("Francisco", "Mathis", "Radiologist")
+    print("---------------------------------------")
+    print("{0}\n{1}\n{2}".format(doc1, doc2, doc3))
+    print("---------------------------------------")
+
+if __name__ == "__main__":
+    main()
